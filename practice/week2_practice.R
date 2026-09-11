4215719bbd74ef63c61ad98174ce1b0f6092b22a
terbear15 <terryxu@upenn.edu>
2026-09-11 01:26:59 -0400

    practice two

--- practice/week2_practice.R
+++ practice/week2_practice.R
@@ -30,3 +30,69 @@ filter(pa_income, estimate > 60000)
 select(pa_income, NAME, estimate, moe)
 
 # Show only GEOID and estimate
+
+mutate(pa_income,moe_pct = moe/estimate*100)
+moe_pct
+pa_income <- mutate(pa_income, moe_pct=moe/estimate*100)
+pa_income
+
+arrange(pa_income, moe_pct)
+arrange(pa_income, desc(moe_pct))
+
+step1 <- filter(pa_income, moe_pct>5)
+step2 <- arrange(step1, desc(moe-pct))
+step3 <- select(step2, NAME, estimate, moe, moe_pct)
+step3
+
+pa_income %>%
+  filter(moe_pct > 5) %>%
+  arrange(desc(moe_pct)) %>%
+  select(NAME, estimate, moe, moe_pct)
+
+# Keep counties with moe_pct over 8, sort by estimate, show NAME and moe_pct
+pa_income %>%
+  filter(moe_pct > 8) %>%
+  arrange(desc(estimate)) %>%
+  select(NAME, moe_pct)
+worst <- pa_income %>%
+  filter(moe_pct > 8) %>%
+  arrange(desc(estimate)) %>%
+  select(NAME, moe_pct)
+
+pa_income <- mutate(pa_income, reliable = moe_pct < 5)
+pa_income %>%
+  group_by(reliable) %>%
+  summarize(n = n(),
+            avg_income = mean(estimate))
+
+pa_income <- pa_income %>%
+  mutate(reliability = case_when(
+    moe_pct < 3 ~ "High confidence",
+    moe_pct < 6 ~ "Moderate",
+    TRUE        ~ "Low confidence"
+  ))
+count(pa_income, reliability)
+
+pa_two <- get_acs(
+  geography = "county",
+  variables = c("B19013_001", "B01003_001"),
+  state = "PA", year = 2023, survey = "acs5"
+)
+
+pa_two
+
+pa_wide <- get_acs(
+  geography = "county",
+  variables = c(income = "B19013_001",
+                pop    = "B01003_001"),
+  state = "PA", year = 2023, survey = "acs5",
+  output = "wide"
+)
+
+pa_wide
+
+pa_wide %>%
+  mutate(moe_pct = incomeM / incomeE * 100) %>%
+  arrange(desc(moe_pct)) %>%
+  select(NAME, popE, incomeE, moe_pct) %>%
+  head(10)
